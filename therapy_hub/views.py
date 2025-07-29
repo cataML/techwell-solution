56e957081da3476a740256693125396087f8b6ce
cataML <catelilungo@gmail.com>
2025-07-29 16:02:37 +0300

    Added About page in Therapy

--- therapy_hub/views.py
+++ therapy_hub/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'therapy_hub/index.html')
+
+def about(request):
+    return render(request, 'therapy_hub/about.html')
