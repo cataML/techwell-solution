764d3584b020e122f585e33ab0c8b171de0baccf
cataML <catelilungo@gmail.com>
2025-07-31 15:27:19 +0300

    Modified therapy app

--- therapy_hub/urls.py
+++ therapy_hub/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import contact_view
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', contact_view, name='contact')
 ]
--- therapy_hub/views.py
+++ therapy_hub/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .forms import ContactForm
+
+
 
 # Create your views here.
 def index(request):
@@ -6,3 +10,17 @@ def index(request):
 
 def about(request):
     return render(request, 'therapy_hub/about.html')
+
+def services(request):
+    return render(request, 'therapy_hub/services.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect(request, 'contact', {'name': form.cleaned_data['name']})
+    else:
+        form = ContactForm()
+        return render(request, 'therapy_hub/contact.html', {'form': form})
